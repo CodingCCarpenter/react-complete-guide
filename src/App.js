f8aa52f6b3915c93e573b70122cfe32ef1e17634
CodingCCarpenter <christineassists@aol.com>
2019-11-04 14:57:05 -0600

    add props to app.js and pass into Person.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <Person />
+        <Person mame='Max' age='28'/>
+        <Person mame='Manu' age='29'>My Hobbies: Racing </Person>
+        <Person mame='Stephanie' age='26'/>
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Person = () => {
+const Person = (props) => {
     return (
         <div>
-            <p>I'm a Person!</p>
+            <p>I'm a Person and I am {Math.floor(Math.random() * 30)} years old!!</p>
         </div>
     )
-}
+};
+
+
 
 export default Person;
