a9488d23544a0569b3f5073566a52dade8ff5bec
CodingCCarpenter <christineassists@aol.com>
2019-11-04 23:31:02 -0600

    saving before starting new lesson

--- src/App.js
+++ src/App.js
@@ -1,50 +1,4 @@
-import React, {useState} from 'react';
-import './App.css';
-import Person from './Person/Person.js'
-
-const app = props => {
-  const [personsState, setPersonsState] = useState({
-    persons: [
-      {name: 'Max', age: 28},
-      {name: 'Manu', age: 29},
-      {name: 'Stephanie', age: 26}
-    ],
-    otherState: 'some other value'
-  });
-
-  const switchNameHandler = () => {
-    // console.log('was clicked!');
-    //DON"T DO THIS this.state.persons[0].name='Maximilian';
-    setPersonsState({
-      persons: [
-        {name: 'Maximilian', age: 28},
-        {name: 'Manu', age: 29},
-        {name: 'Stephanie', age: 27}
-      ]
-    });
-    const [otherState, setOtherState] = useState('some other value');
-  }
-
-    console.log(personsState);
-  
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
-        <button onClick={switchNameHandler}>Switch Name</button>
-        <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-        <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My Hobbies: Racing </Person>
-        <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
-      </div>
-    );
-  }
-
-
-export default app;
-
-
-//original code 
-/* 
+//stateful, smart, container
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person.js'
@@ -59,12 +13,12 @@ class App extends Component {
     otherState: 'some other value'
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = ( newName ) => {
     // console.log('was clicked!');
     //DON"T DO THIS this.state.persons[0].name='Maximilian';
     this.setState({
       persons: [
-        {name: 'Maximilian', age: 28},
+        {name: newName, age: 28},
         {name: 'Manu', age: 29},
         {name: 'Stephanie', age: 27}
       ]
@@ -76,14 +30,25 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+        <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age}
+        />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}>
+            My Hobbies: Racing 
+        </Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age}
+        />
       </div>
     );
   }
 }
 
 export default App;
-*/
+
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,9 +1,12 @@
+//presentational, stateless or dumb component- means the component does not manage state.
+    //you want to have as many components be stateless as possible.
+    //this allows the page to render more quickly, and allows the code to be much easier to manage.
 import React from 'react';
 
 const Person = ( props ) => {
     return (
         <div>
-            <p>I'm {props.name} and I am {props.age} years old!!</p>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!!</p>
             {/* props.children includes everything between opening and closing tag. For example <p>this is where the children live</p> */}
             <p>{props.children}</p>
         </div>
