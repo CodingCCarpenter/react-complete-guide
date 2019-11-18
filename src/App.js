24001d275243ab9209eae0663be4344f4caa1f54
CodingCCarpenter <christineassists@aol.com>
2019-11-17 20:46:56 -0600

    test commit

--- src/App.js
+++ src/App.js
@@ -14,23 +14,45 @@ class App extends Component {
   }
 
   switchNameHandler = ( newName ) => {
-    console.log('was clicked!');
+    console.log('cc: src/App.js :App:switchNameHandler was called!');
     //DON"T DO THIS: this.state.persons[0].name='Maximilian';
     this.setState({
       persons: [
-        {name: newName, age: 28},
+        {name: 'newName', age: 28},
         {name: 'Manu', age: 29},
         {name: 'Stephanie', age: 27}
       ]
     })
   }
 
+  nameChangedHandler = (event) => {
+    console.log('cc: src/App.js :App:nameChangedHandler was called!');
+    this.setState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 29},
+        {name: 'Stephanie', age: 26}
+      ]
+    })
+
+  }
+
   render() {
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
-        <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button>
+        <button 
+          style={style} 
+          onClick={ () =>  this.switchNameHandler('Maximilian!!') }>Switch Name</button> {/*can be inefficient. Use bind instead*/}
         <Person 
           name={ this.state.persons[0].name } 
           age={ this.state.persons[0].age }
@@ -38,7 +60,8 @@ class App extends Component {
         <Person 
           name={ this.state.persons[1].name } 
           age={ this.state.persons[1].age }
-          click={ this.switchNameHandler.bind(this, 'Max!') }>
+          click={ this.switchNameHandler.bind(this, 'Max!') } 
+          changed={ this.nameChangedHandler } >
             My Hobbies: Racing 
         </Person>
         <Person 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,13 +2,20 @@
     //you want to have as many components be stateless as possible.
     //this allows the page to render more quickly, and allows the code to be much easier to manage.
 import React from 'react';
+import './Person.css';
+
 
 const Person = ( props ) => {
     return (
-        <div>
-            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!!</p>
+        <div className='Person'>
+            <p onClick={ props.click }>I'm { props.name } and I am { props.age } years old!!</p>
             {/* props.children includes everything between opening and closing tag. For example <p>this is where the children live</p> */}
-            <p>{props.children}</p>
+            <p>{ props.children }</p>
+            <input 
+                type='text' 
+                onChange={ props.changed } 
+                value={ props.name } 
+            />
         </div>
     )
 };
